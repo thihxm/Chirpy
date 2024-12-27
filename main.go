6a793f700c0d2003b247b97b9a06cbae64c380d6
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2024-12-27 18:04:56 -0300

    feat: file server

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import "net/http"
 
 func main() {
 	handler := http.NewServeMux()
+
+	handler.Handle("/", http.FileServer(http.Dir(".")))
+
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: handler,
