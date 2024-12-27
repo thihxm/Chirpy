01efb82cb298562f41fbcbdc856a5dae74bbfe1b
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2024-12-27 18:09:10 -0300

    feat: added assets path

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ func main() {
 	handler := http.NewServeMux()
 
 	handler.Handle("/", http.FileServer(http.Dir(".")))
+	handler.Handle("/assets", http.FileServer(http.Dir("./assets")))
 
 	server := &http.Server{
 		Addr:    ":8080",
