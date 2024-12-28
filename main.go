4dd94c3bca123023a62f8bbc5047826fc2f3b665
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2024-12-28 16:22:06 -0300

    feat: file server hits middleware

--- main.go
+++ main.go
@@ -1,25 +1,54 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
-func main() {
-	handler := http.NewServeMux()
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
-	handler.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+func main() {
+	mux := http.NewServeMux()
+	apiCfg := &apiConfig{}
+
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
-	handler.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/reset", apiCfg.reset)
+	mux.HandleFunc("/metrics", apiCfg.metrics)
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
 
 	server := &http.Server{
 		Addr:    ":8080",
-		Handler: handler,
+		Handler: mux,
 	}
 
 	log.Printf("Server started at %s", server.Addr)
 	log.Fatal(server.ListenAndServe())
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) reset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
