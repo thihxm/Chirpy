ff1d1616f4606f720e01afb817e39fe31802f890
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2024-12-28 16:53:42 -0300

    feat: api validate message length

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -28,6 +29,45 @@ func main() {
 		Addr:    ":8080",
 		Handler: mux,
 	}
+
+	mux.Handle("/api/validate_chirp", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		type parameters struct {
+			Body string `json:"body"`
+		}
+
+		decoder := json.NewDecoder(r.Body)
+		params := parameters{}
+		err := decoder.Decode(&params)
+		if err != nil {
+			log.Printf("Error decoding parameters: %v", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		type returnVals struct {
+			// the key will be the name of struct field unless you give it an explicit JSON tag
+			Valid bool `json:"valid"`
+		}
+
+		resBody := returnVals{
+			Valid: len(params.Body) <= 140,
+		}
+		data, err := json.Marshal(resBody)
+		if err != nil {
+			log.Printf("Error marshalling response JSON: %v", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		if len(params.Body) > 140 {
+			w.WriteHeader(http.StatusBadRequest)
+		} else {
+			w.WriteHeader(http.StatusOK)
+		}
+
+		w.Header().Add("Content-Type", "application/json; charset=utf-8")
+		w.Write(data)
+	}))
 
 	log.Printf("Server started at %s", server.Addr)
 	log.Fatal(server.ListenAndServe())
